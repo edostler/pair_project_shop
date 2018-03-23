529583f07b534eea973e5b6c16ce5e2e6d486a85
daniel kelly <danieljkelly91@gmail.com>
2018-03-23 15:53:39 +0000

    adds property to food class and getters and setters

--- src/main/java/models/Food.java
+++ src/main/java/models/Food.java
@@ -1,4 +1,24 @@
 package models;
 
-public class Food {
+import java.util.GregorianCalendar;
+
+public class Food extends Product {
+
+    private FoodCategory category;
+
+    public Food() {
+    }
+
+    public Food(String name, FoodCategory category, double price, int quantity, String description, GregorianCalendar stockDate) {
+        super(name, price, quantity, description, stockDate);
+        this.category = category;
+    }
+
+    public FoodCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(FoodCategory category) {
+        this.category = category;
+    }
 }
