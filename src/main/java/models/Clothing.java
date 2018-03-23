795f5d14d518f5089a272a4e0eb228e455ce808d
daniel kelly <danieljkelly91@gmail.com>
2018-03-23 16:09:22 +0000

    adds constructor to clothing class, getter and setter for clothing category

--- src/main/java/models/Clothing.java
+++ src/main/java/models/Clothing.java
@@ -1,4 +1,24 @@
 package models;
 
-public class Clothing {
+import java.util.GregorianCalendar;
+
+public class Clothing extends Product {
+
+    private ClothingCategory category;
+
+    public Clothing() {
+    }
+
+    public Clothing(String name, ClothingCategory category, double price, int quantity, String description, GregorianCalendar stockDate) {
+        super(name, price, quantity, description, stockDate);
+        this.category = category;
+    }
+
+    public ClothingCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ClothingCategory category) {
+        this.category = category;
+    }
 }
