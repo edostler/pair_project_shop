6de4f176b58dc71356c1c3b22578d01476f34b75
Ed Ostler <edward.o@simul8.com>
2018-03-23 13:49:23 +0000

    Adds declarations, constructor,  and getters/setters for parent class Order as well as basic tests in OrderTest

--- src/main/java/models/Order.java
+++ src/main/java/models/Order.java
@@ -1,4 +1,50 @@
 package models;
 
-public class Order {
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class Order {
+
+    private int id;
+    private double total;
+    private Set<Product> contents;
+    private User user;
+
+    public Order(User user) {
+        this.total = 0;
+        this.user = user;
+        this.contents = new HashSet<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public Set<Product> getContents() {
+        return contents;
+    }
+
+    public void setContents(Set<Product> contents) {
+        this.contents = contents;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/test/java/ClassTests/OrderTest.java
+++ src/test/java/ClassTests/OrderTest.java
@@ -1,9 +1,31 @@
 package ClassTests;
 
+import models.Order;
+import models.User;
 import org.junit.Before;
 import org.junit.Test;
 
-public class OrderTest {
-    
+import static org.junit.Assert.assertEquals;
 
+public class OrderTest {
+
+    private Order order;
+    private User user;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new User("Ed", "edostler", 25);
+        order = new Order(user);
+    }
+
+    @Test
+    public void testTotal() {
+        assertEquals(0, order.getTotal(), 0.01);
+    }
+
+    @Test
+    public void testSetTotal() {
+        order.setTotal(10.55);
+        assertEquals(10.55, order.getTotal(), 0.01);
+    }
 }
