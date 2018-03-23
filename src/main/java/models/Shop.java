539bdd47622cda39e8e29e53406e3471e27fb40c
Ed Ostler <edward.o@simul8.com>
2018-03-23 12:02:17 +0000

    Adds Shop class decalrations, constructor and getter/setters and ShopTest getName test.

--- src/main/java/models/Shop.java
+++ src/main/java/models/Shop.java
@@ -1,4 +1,41 @@
 package models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Shop {
+
+    private int id;
+    private String name;
+    private Set<Order> allOrders;
+
+    public Shop(String name) {
+        this.name = name;
+        this.allOrders = new HashSet<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Order> getAllOrders() {
+        return allOrders;
+    }
+
+    public void setAllOrders(Set<Order> allOrders) {
+        this.allOrders = allOrders;
+    }
+
 }
