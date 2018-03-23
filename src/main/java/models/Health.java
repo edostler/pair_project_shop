21d0b8ffa7bdfd1cd4756d7221a0d5b3444ce20c
daniel kelly <danieljkelly91@gmail.com>
2018-03-23 15:59:41 +0000

    adds healthcareCategory to health class, set up contructor and getter and setter

--- src/main/java/models/Health.java
+++ src/main/java/models/Health.java
@@ -1,4 +1,24 @@
 package models;
 
-public class Health {
+import java.util.GregorianCalendar;
+
+public class Health extends Product {
+
+    private HealthCategory category;
+
+    public Health() {
+    }
+
+    public Health(String name, HealthCategory category, double price, int quantity, String description, GregorianCalendar stockDate) {
+        super(name, price, quantity, description, stockDate);
+        this.category = category;
+    }
+
+    public HealthCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(HealthCategory category) {
+        this.category = category;
+    }
 }
