70a832d6deab938dafdeea576cd4f25efb1cff30
daniel kelly <danieljkelly91@gmail.com>
2018-03-23 11:20:09 +0000

    adds user class with properties and getters and setters for all

--- src/test/java/ClassTests/UserTest.java
+++ src/test/java/ClassTests/UserTest.java
@@ -1,4 +1,19 @@
 package ClassTests;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class UserTest {
+
+    User user;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new User();
+    }
+
+    @Test
+    public void testName() {
+        assertEquals("");
+    }
 }
