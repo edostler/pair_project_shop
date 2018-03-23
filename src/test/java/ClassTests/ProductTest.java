ff0cea65bc921795c56f073c7008cf6ecee98f61
daniel kelly <danieljkelly91@gmail.com>
2018-03-23 15:38:34 +0000

    adds product class and tests for each property

--- src/test/java/ClassTests/ProductTest.java
+++ src/test/java/ClassTests/ProductTest.java
@@ -7,14 +7,18 @@ import org.junit.Test;
 import java.util.GregorianCalendar;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ProductTest {
 
     Product product;
+    Product product2;
 
     @Before
     public void setUp() throws Exception {
-        product = new Product("milk", 1.99, true, 1, "2l Semi-Skimmed", new GregorianCalendar(2018,3,23));
+        product = new Product("milk", 1.99, 1, "2l Semi-Skimmed", new GregorianCalendar(2018,3,30));
+        product2 = new Product("bread", 1.99, 0, "Warbutons wholemeal", new GregorianCalendar(2018,3,23));
     }
 
     @Test
@@ -23,18 +27,43 @@ public class ProductTest {
     }
 
     @Test
+    public void testSetName() {
+        product.setName("soy milk");
+        assertEquals("soy milk", product.getName());
+    }
+
+    @Test
     public void testPrice() {
-        assertEquals(1.99, product.getPrice());
+        assertEquals(1.99, product.getPrice(), 0.01);
+    }
+
+    @Test
+    public void testSetPrice(){
+        product.setPrice(1.50);
+        assertEquals(1.50, product.getPrice(), 0.01);
+
+
     }
 
     @Test
     public void testAvailability() {
-        assertEquals(true, product.getAvailabilty());
+        assertTrue(product.getAvailability());
+        assertFalse(product2.getAvailability());
     }
+
 
     @Test
     public void testQuantity() {
         assertEquals(1, product.getQuantity());
+    }
+
+    @Test
+    public void testSetQuantity() {
+        product2.setQuantity(5);
+        assertEquals(5,product2.getQuantity());
+        assertTrue(product2.getAvailability());
+
+
     }
 
     @Test
@@ -43,8 +72,18 @@ public class ProductTest {
     }
 
     @Test
-    public void testStockDate() {
-        assertEquals(new GregorianCalendar(2018,3,23), product.getDate());
+    public void testSetDescription() {
+        product.setDescription("3l Semi-Skimmed");
+        assertEquals("3l Semi-Skimmed", product.getDescription());
     }
+
+    @Test
+    public void testStockDate() {
+        assertEquals(2018, product.getStockDate().get(GregorianCalendar.YEAR));
+        assertEquals(3, product.getStockDate().get(GregorianCalendar.MONTH));
+        assertEquals(30, product.getStockDate().get(GregorianCalendar.DAY_OF_MONTH));
+    }
+
+
 }
 
