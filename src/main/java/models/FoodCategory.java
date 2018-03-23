eecb6c0f604e7ed0ef03696920be5f603dc54e50
daniel kelly <danieljkelly91@gmail.com>
2018-03-23 11:02:09 +0000

    adds food categories

--- src/main/java/models/FoodCategory.java
+++ src/main/java/models/FoodCategory.java
@@ -1,4 +1,13 @@
 package models;
 
+import org.apache.velocity.util.introspection.UberspectImpl;
+
 public enum FoodCategory {
+    BREAD,
+    CEREALS,
+    DAIRY,
+    DRINK,
+    FROZEN,
+    FRUIT_AND_VEG,
+    MEAT
 }
