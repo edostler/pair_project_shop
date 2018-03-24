e9b2db08d28d1e16d396ac5749d4adef1b015aa9
daniel kelly <danieljkelly91@gmail.com>
2018-03-24 22:14:55 +0000

    adds roots in foodProducts controller

--- src/main/java/controllers/FoodProductsController.java
+++ src/main/java/controllers/FoodProductsController.java
@@ -1,4 +1,47 @@
 package controllers;
 
+import db.DBHelper;
+import models.Food;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+
 public class FoodProductsController {
+
+    public FoodProductsController() {
+        this.setupEndPoints();
+    }
+
+    private void setupEndPoints() {
+        get("/food-products", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Food> foods = DBHelper.getAll(Food.class);
+            model.put("foods", foods);
+            model.put("template", "templates/foodProducts/index.vtl");
+            String loggedInUser = LoginController.getLoggedInUserName(req, res);
+            model.put("user", loggedInUser);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/food-products/:id", (req, res) -> {
+            String strId = req.params(":id");
+            Integer intId = Integer.parseInt(strId);
+            Food food = DBHelper.find(Food.class, intId);
+
+            Map<String, Object> model = new HashMap<>();
+
+            model.put("food", food);
+            model.put("template", "templates/foodProducts/show.vtl");
+            String loggedInUser = LoginController.getLoggedInUserName(req, res);
+            model.put("user", loggedInUser);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+    }
 }
