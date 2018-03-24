9596f5691f0825cd6f80823d4b56d8dea2699c34
daniel kelly <danieljkelly91@gmail.com>
2018-03-24 22:16:12 +0000

    adds roots to products controller

--- src/main/java/controllers/ProductsController.java
+++ src/main/java/controllers/ProductsController.java
@@ -1,4 +1,50 @@
 package controllers;
 
+import db.DBHelper;
+import models.Product;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+
 public class ProductsController {
+
+    public ProductsController() {
+        this.setupEndPoints();
+    }
+
+    private void setupEndPoints() {
+        get("/products", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Product> products = DBHelper.getAll(Product.class);
+            model.put("products", products);
+            model.put("template","templates/products/index.vtl");
+
+            String loggedInUser = LoginController.getLoggedInUserName(req, res);
+            model.put("user", loggedInUser);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/products/:id", (req, res) -> {
+            String strId = req.params(":id");
+            Integer intId = Integer.parseInt(strId);
+            Product product = DBHelper.find(Product.class, intId);
+
+            Map<String, Object> model = new HashMap<>();
+
+            model.put("product", product);
+            model.put("template", "templates/products/show.vtl");
+            String loggedInUser = LoginController.getLoggedInUserName(req, res);
+            model.put("user", loggedInUser);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+
+    }
 }
