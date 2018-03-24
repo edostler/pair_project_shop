a1028eadc25bcfaa054727f23f8dfaa4e1ddac31
Ed Ostler <edward.o@simul8.com>
2018-03-24 14:26:15 +0000

    Adds all basic content for MainController with Heroku elements commented out

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,4 +1,53 @@
 package controllers;
 
+import db.Seeds;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.SparkBase.port;
+import static spark.SparkBase.staticFileLocation;
+
 public class MainController {
+
+    public static void main(String[] args) {
+
+//        port(getHerokuAssignedPort());
+
+        Seeds.seedData();
+
+        staticFileLocation("/public");
+
+        LoginController loginController = new LoginController();
+        ShopsController shopsController = new ShopsController();
+        UsersController usersController = new UsersController();
+        PurchasesController purchasesController = new PurchasesController();
+        PreviousPurchasesController previousPurchasesController = new PreviousPurchasesController();
+        CurrentPurchasesController currentPurchasesController = new CurrentPurchasesController();
+        ProductsController productsController = new ProductsController();
+        FoodProductsController foodProductsController = new FoodProductsController();
+        ClothingProductsController clothingProductsController = new ClothingProductsController();
+        HealthProductsController healthProductsController = new HealthProductsController();
+
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String loggedInUser = LoginController.getLoggedInUserName(req, res);
+            model.put("user", loggedInUser);
+            model.put("template","templates/main.vtl");
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+    }
+
+//    static int getHerokuAssignedPort() {
+//        ProcessBuilder processBuilder = new ProcessBuilder();
+//        if (processBuilder.environment().get("PORT") != null) {
+//            return Integer.parseInt(processBuilder.environment().get("PORT"));
+//        }
+//        return 4567;
+//    }
+
 }
