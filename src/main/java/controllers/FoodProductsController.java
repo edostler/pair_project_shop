b864941ec181e8eef398259d3d41bb126293fc0b
daniel kelly <danieljkelly91@gmail.com>
2018-03-25 17:35:09 +0100

    adds roots to foodController

--- src/main/java/controllers/FoodProductsController.java
+++ src/main/java/controllers/FoodProductsController.java
@@ -2,14 +2,19 @@ package controllers;
 
 import db.DBHelper;
 import models.Food;
+import models.FoodCategory;
+import models.Product;
+import models.Shop;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class FoodProductsController {
 
@@ -18,6 +23,26 @@ public class FoodProductsController {
     }
 
     private void setupEndPoints() {
+
+
+
+
+        get("/food-products/:id/edit", (req, res) -> {
+            String strId = req.params(":id");
+            Integer intId = Integer.parseInt(strId);
+            Food food = DBHelper.find(Food.class, intId);
+            List<Shop> shops = DBHelper.getAll(Shop.class);
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("shops", shops);
+            model.put("food", food);
+            model.put("template", "templates/foodProducts/edit.vtl");
+            String loggedInUser = LoginController.getLoggedInUsername(req, res);
+            model.put("user", loggedInUser);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
         get("/food-products", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             List<Food> foods = DBHelper.getAll(Food.class);
@@ -28,6 +53,17 @@ public class FoodProductsController {
 
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
+
+        get ("/food-products/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Shop> shops = DBHelper.getAll(Shop.class);
+            model.put("shops", shops);
+            model.put("template", "templates/foodProducts/create.vtl");
+            String loggedInUser = LoginController.getLoggedInUsername(req, res);
+            model.put("user", loggedInUser);
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
 
         get("/food-products/:id", (req, res) -> {
             String strId = req.params(":id");
@@ -43,5 +79,52 @@ public class FoodProductsController {
 
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
+
+
+
+
+//        post ("/food-products", (req, res) -> {
+//            int shopId = Integer.parseInt(req.queryParams("shop"));
+//            Shop shop = DBHelper.find(Shop.class, shopId);
+//            String name = req.queryParams("name");
+//            String description = req.queryParams("description");
+//            int quantity = Integer.parseInt(req.queryParams("quantity"));
+//            double price = Double.parseDouble(req.queryParams("price"));
+//            String strStockDate = req.queryParams("stockDate");
+//            GregorianCalendar stockDate = DBHelper.formatStringToDate(strStockDate);
+//            Food food = new Food(name, category, price, quantity, description, stockDate, shop);
+//            DBHelper.saveOrUpdate(food);
+//            res.redirect("/customers");
+//            return null;
+//        }, new VelocityTemplateEngine());
+
+        post ("/food-products/:id/delete", (req, res) -> {
+            int id = Integer.parseInt(req.params(":id"));
+            Food productToDelete = DBHelper.find(Food.class, id);
+            DBHelper.delete(productToDelete);
+            res.redirect("/food-products");
+            return null;
+        }, new VelocityTemplateEngine());
+
+//        post ("/products/:id", (req, res) -> {
+//            String strId = req.params(":id");
+//            Integer intId = Integer.parseInt(strId);
+//            Product product = DBHelper.find(Product.class, intId);
+//            int shopId = Integer.parseInt(req.queryParams("shop"));
+//            Shop shop = DBHelper.find(Shop.class, shopId);
+//            String name = req.queryParams("name");
+//            String username = req.queryParams("username");
+//            int distance = Integer.parseInt(req.queryParams("distance"));
+//
+//            customer.setName(name);
+//            customer.setUsername(username);
+//            customer.setDistance(distance);
+//            customer.setShop(shop);
+//            DBHelper.saveOrUpdate(customer);
+//            res.redirect("/customers");
+//            return null;
+//
+//        }, new VelocityTemplateEngine());
+    }
     }
 }
