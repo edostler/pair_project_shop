023bad1d03ffaaa9a4e0d507cb337711abbe724f
daniel kelly <danieljkelly91@gmail.com>
2018-03-25 02:17:06 +0100

    adds form to users/create.vtl

--- src/main/java/controllers/UsersController.java
+++ src/main/java/controllers/UsersController.java
@@ -1,4 +1,33 @@
 package controllers;
 
+import db.DBHelper;
+import models.User;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+
 public class UsersController {
+
+    public UsersController() {
+        this.setupEndPoints();
+    }
+
+    private void setupEndPoints() {
+
+
+        get("/account", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+
+            String loggedInUser = LoginController.getLoggedInUserName(req, res);
+            model.put("user", loggedInUser);
+            model.put("template", "templates/users/index.vtl");
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+    }
 }
